c506669896c9dc28f983b826573bf15d27b7c6b7
Jenise Howden <130766195+jhowdenSA@users.noreply.github.com>
2025-03-27 12:59:32 -0400

    Add wordy exercise

--- breakout_2/ATP_starter/js-jest/wordy/wordy.js
+++ breakout_2/ATP_starter/js-jest/wordy/wordy.js
@@ -1,8 +1,16 @@
-//
 // This is only a SKELETON file for the 'Wordy' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
 
-export const answer = () => {
-  throw new Error('Remove this statement and implement this function');
+export const answer = (question) => {
+    var strArray = question.split(" ");
+    var totalValue = 0;
+    var currentOperation;
+    for (var i = 2; i < strArray.length; i++) {
+        if (i % 2 === 0) {
+            strArray[i] = strArray[i].replace("?", "");
+            return parseInt(strArray[i]);
+        } else {
+        }
+    }
 };
